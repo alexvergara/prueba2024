c8621def1ecf24c2a468978adf4aee9177eef8b9
Alex Vergara <alex.vergara@gmail.com>
2024-04-27 11:46:53 -0500

    Reorganize databse init files
    Rename files for execution on container initialization

--- database/migrate.sh
+++
@@ -1,4 +0,0 @@
-# Execute the SQL migrations to create the database schema.
-for file in /var/www/html/database/migrations/*.sql; do
-    mysql -h db -u user -ppassword prueba2024 < $file
-done
---
+++ database/sqls/fix-native-pass.sql
@@ -0,0 +1,2 @@
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY '';
+ALTER USER 'user'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
