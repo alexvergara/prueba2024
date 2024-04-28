8917cfdc43f51a324d840e6321b7ce981295d3d3
Alex Vergara <alex.vergara@gmail.com>
2024-04-28 10:48:12 -0500

    Add ErrorHandler class
    Create and enable app/Core/ErrorHander class

---
+++ app/Core/ErrorHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+class ErrorHandler
+{
+
+    /**
+     * Register the error handler
+     * @param Throwable $exception
+     * @return void
+     */
+    public static function handleException(Throwable $exception): void
+    {
+        http_response_code(500);
+
+        echo json_encode([
+            "code" => $exception->getCode(),
+            "message" => $exception->getMessage(),
+            "file" => $exception->getFile(),
+            "line" => $exception->getLine()
+        ]);
+    }
+
+    /**
+     * Handle PHP errors
+     * @param int $errno
+     * @param string $errstr
+     * @param string $errfile
+     * @param int $errline
+     * @return bool
+     */
+    public static function handleError(int $errno, string $errstr, string $errfile, int $errline): bool
+    {
+        throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+    }
+}
--- app/Core/bootstrap.php
+++ app/Core/bootstrap.php
@@ -1,9 +1,13 @@
 <?php
 
+require_once 'ErrorHandler.php';
+
+// Set the error handler
+set_error_handler("ErrorHandler::handleError");
+set_exception_handler("ErrorHandler::handleException");
+
 // Load the composer autoloader
 require '../vendor/autoload.php';
 
-//set_error_handler("ErrorHandler::handleError");
-//set_exception_handler("ErrorHandler::handleException");
-
+// Load routes file
 require '../routes/api.php';
